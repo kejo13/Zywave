8526ad8f219632a9abd85949f089818b1cc1b455
kejo <kj.insigne@gmail.com>
2024-04-01 11:05:30 +0800

    Add project files.

---
+++ Zywave/Controllers/EmailFilterController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zywave.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailFilterController : ControllerBase
+    {
+        private readonly EmailFilterService _emailFilterService;
+
+        public EmailFilterController(EmailFilterService emailFilterService)
+        {
+            _emailFilterService = emailFilterService;
+        }
+
+        [HttpPost]
+        public ActionResult<(bool isClassified, string censoredText)> FilterEmail (EmailFilterRequest request)
+        {
+            var result = _emailFilterService.FilterEmail(request.ClassifiedWords, request.EmailText);
+            return Ok(new { isClassified = result.isClassified, censoredText = result.censoredText});
+        }
+
+        public class EmailFilterRequest
+        {
+            public required List<string> ClassifiedWords { get; set;}
+            public required string EmailText { get; set;}
+        }
+    }
+}
---
+++ Zywave/EmailFilterService.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Zywave
+{
+    public class EmailFilterService
+    {
+        public (bool isClassified, string censoredText) FilterEmail(List<string> classifiedWords, string emailText)
+        {
+            bool isClassified = false;
+            string censoredText = emailText;
+
+            foreach (string word in classifiedWords)
+            {
+                if (emailText.Contains(word, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    isClassified = true;
+                    censoredText = Regex.Replace(censoredText, "\\b" + word + "\\b", "*****", RegexOptions.IgnoreCase);
+                }
+            }
+
+            return (isClassified, censoredText);
+        }
+    }
+}
